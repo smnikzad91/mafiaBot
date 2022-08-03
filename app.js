79bfb087d0091360400a9a973ee54f845cbfaa4e
dev-shayan <smnikzad91@gmail.com>
2022-08-03 10:09:40 +0430

    test

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(5051, console.log(`http server is running`))
+app.listen(5051, console.log(`http server is running ha ha`))
 
 module.exports = app;
